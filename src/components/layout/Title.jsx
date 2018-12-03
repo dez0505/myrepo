307d8808162e042ad2c10b41684f6c1f74eab5d3
dez0514 <2573433803@qq.com>
2018-12-03 22:16:11 +0800

    1

--- src/components/layout/Title.jsx
+++ src/components/layout/Title.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Title.scss'
 class Title extends Component {
     constructor(props) {
       super(); //可以不给props
@@ -10,7 +11,7 @@ class Title extends Component {
       return (
         <div className='Title-box'>
           <img src="../../images/home/title.png" alt=""/>
-          <div>{this.props.title}</div>
+          <div className='txt'>{this.props.title}</div>
         </div>
       ) 
     }
