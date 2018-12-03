4ffbda76753b54276c00d3e04a038881e8d744e5
18755489161@163.com <18755489161@163.com>
2018-12-03 22:11:35 +0800

    adsSwiper  edit

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -5,38 +5,52 @@ import '../../styles/homeTheme.scss'
 // subComponent
 import Header from '../../components/layout/Header'
 import Nav from '../../components/layout/Nav'
+import AdsSwiper from './subComponents/AdsSwiper'
+
+// api
+import { getHomeData } from '../../api/home'
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      topLineList:[
-        {name:'zuowang',age:23},
-        {name:'zhangyang',age:22},
-        {name:'chenhao',age:21},
-      ]
+      adsListData:[],
+      liveListData:[],
+      noticeListData:[],
+      topicListData:[]
     }
   }
   componentDidMount(){
-    console.log('props', this.props)
     const random = Math.floor(Math.random()*2);
-    console.log(random)
     if(random === 1){
       this.props.updateTheme({theme:'white'})
     }else{
       this.props.updateTheme({theme:'black'})
     }
-    console.log(this.props.theme)
-    // this.setState({addTodoList:this.props.todos()})
+    this.getHomeDate()
   }
-  updateList = (val) =>{
-    this.setState({addTodoList:this.props.todos()})
-    this.props.updateList(this.state.topLineList)
+  async getHomeDate () {
+   const  { data }  =await getHomeData({
+     theme: this.props.theme === 'white' ? 'day' : this.props.theme === 'black'? 'night' : 'red',
+     version: '7.00',
+     platform: 'android'
+   })
+   this.setState({
+    adsListData:data.FirstAppAdsJson,
+    noticeListData:data.AnnounceJson,
+    liveListData:data.LivePicsJson,
+    topicListData:data.topicListData
+   })
   }
+  // updateList = (val) =>{
+  //   this.setState({addTodoList:this.props.todos()})
+  //   this.props.updateList(this.state.topLineList)
+  // }
   render() {
     return (
       <div className={`home-warpper ${this.props.theme==='white'?'white':'black'}`}>
         <Header/>
         <Nav/>
+        <AdsSwiper adsList = {this.state.adsListData}/>
       </div>
     ) 
   }
--- src/views/home/subComponents/AdsSwiper.jsx
+++ src/views/home/subComponents/AdsSwiper.jsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
+// Swiper
 import './AdsSwiper.scss'
 import Swiper from 'swiper'
-import 'swiper/dist/css/swiper.min.scss'
+import 'swiper/dist/css/swiper.min.css'
+
+// api
+
 class AdsSwiper extends Component {
   constructor(props) {
     super(); //可以不给props
+    this.state = {
+      mySwiper: null
+    }
   }
   componentDidMount() {
     console.log('adsSwiper', this.props)
+    this.setState({ mySwiper: new Swiper('.ads-swiper', {
+      autoplay: true,//可选选项，自动滑动
+      loop: true,
+    }) })
   }
   render() {
     return (
-      <div class="swiper-container">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
+      <div className="ads-box">
+        <div className="swiper-container ads-swiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide" >Slide 1</div>
+            <div className="swiper-slide">Slide 2</div>
+            <div className="swiper-slide">Slide 3</div>
+          </div>
+          <div className="swiper-pagination"></div>
         </div>
-        <div class="swiper-pagination"></div>
       </div>
     ) 
   }
