0cee84ddca71bb8d2fea88a6d9861b017c04732b
左旺 <zuowang@zuowangdeMacBook-Pro.local>
2018-12-25 22:45:41 +0800

    home主要的的逻辑提取到公共的js文件中

--- src/actions/home.js
+++ src/actions/home.js
@@ -5,13 +5,15 @@ import getLiveList from './tab/live'
 import { getNewsList, getOptionalList } from './tab/optional'
 // actions
 import { updateTabIndex } from './tab'
+import { updatePageConfig } from './index'
 import { updateInterfaceState, resetState, updateLoadingState } from './list'
 // utils
 import { getManDianParams, getQueryString } from '../utils/common'
 
 // type : optional  home
 // index : 切换的下标 
-export default function updateTabIndexCallBack(index, type) {
+// hasMandian 是否执行埋点
+export default function updateTabIndexCallBack(index, type, hasMandian = true) {
   return  (dispatch, getState) => {
     const activeType = type === 'home' ? 'activeHomeTabIndex' : 'activeOptionalTabIndex'
     const activeTabIndex = getState().tab.tabIndexData[activeType]
@@ -19,11 +21,16 @@ export default function updateTabIndexCallBack(index, type) {
     if(index === activeTabIndex) return
     // 1、清空所有状态
     dispatch(resetState())
+    // 监听是否移到起初页
+    const scrollToStartPosition = getState().pageConfig.scrollToStartPosition
+    dispatch(updatePageConfig({scrollToStartPosition}))
     // 2、增加埋点
-    if(type==='home') {
-      _goToTabFunction('tab' + index)
-    } else {
-      _goToTabFunction('optional' + index)
+    if(hasMandian) {
+      if(type==='home') {
+        _goToTabFunction('tab' + index)
+      } else {
+        _goToTabFunction('optional' + index)
+      }
     }
     // 3、切换到当前index
     dispatch(updateTabIndex({[activeType]:index}))
@@ -34,10 +41,37 @@ export default function updateTabIndexCallBack(index, type) {
     // 6 根据whichLoading掉接口
     const activeWhichLoading = getState().list.interfaceState.whichLoading
     _getTabList(activeWhichLoading, 'init', dispatch)
-    dispatch(updateInterfaceState())
-    if(activeTabIndex) {
-
-    }
+  }
+}
+// 加载更多列表数据
+export function loadListEvent() {
+  return (dispatch, getState) => {
+    // 0 如果正在加载中，则不加载数据
+    const loadLoading = getState().list.loadingState.loadLoading
+    if(loadLoading) return
+    // 1 加载中状态
+    dispatch(updateLoadingState({loadLoading: true}))
+    // 2、调用哪个接口
+    const activeWhichLoading = getState().list.interfaceState.whichLoading
+    _getTabList(activeWhichLoading, 'load', dispatch)
+  }
+}
+// 刷新列表数据
+export function refreshListEvent() {
+  return (dispatch, getState) => {
+    // 0 如果状态正在加载中，则不刷新
+    const activeWhichLoading = getState().list.interfaceState.whichLoading
+    const refreshLoading = getState().list.loadingState.refreshLoading
+    if(refreshLoading || activeWhichLoading === 'more') return
+    // 1、跳回list顶部
+    const scrollToStartPosition = getState().pageConfig.scrollToStartPosition
+    dispatch(updatePageConfig({scrollToStartPosition}))
+    // 2  清空状态
+    dispatch(resetState())
+    // 3  改变加载中状态
+    dispatch(updateLoadingState({refreshLoading: true}))
+    // 4  根据whichLoading 调用哪个接口
+    _getTabList(activeWhichLoading, 'init', dispatch)
   }
 }
 // index 根据下标进行更换whichLoading
@@ -58,7 +92,7 @@ function _updateWhichLoading(index, type, dispatch, getState) {
         this.props.updateInterfaceState({whichLoading: 'liveA'})
         break;
       case 3:
-        dispatch.updateTabIndex({activeOptionalTabIndex: 0}) 
+        dispatch.updateTabIndexCallBack(0, 'optional', false) 
         break;
       case 4:
         this.props.updateInterfaceState({whichLoading:'more'})
@@ -88,7 +122,7 @@ function _updateWhichLoading(index, type, dispatch, getState) {
     }
   }
 }
-
+// 埋点
 function _goToTabFunction (tabIndex) {
   let clickParam = getManDianParams(tabIndex)
   if (getQueryString('platform').toLowerCase().indexOf('iphone') >= 0) {
@@ -99,7 +133,7 @@ function _goToTabFunction (tabIndex) {
     window.location.href = 'ehtsec://logNative?' + clickParam.slice(1)
   }
 }
-
+// 调接口，得到listData
 function _getTabList(whichLoading, type = 'init', dispatch) {
   switch (whichLoading) {
     case 'topLine':
